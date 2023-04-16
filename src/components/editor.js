b123ad2bd28d7515dd53079e9da497410b75e03f
Ndhote Ilyas <ii.ndhote@gmail.com>
2023-04-16 07:58:01 +0000

    innitial set up

---
+++ src/components/editor.js
@@ -0,0 +1,294 @@
+'use client'
+
+import React, { useState, useEffect, useRef } from "react";
+import {zodResolver} from '@hookform/resolvers/zod'
+import * as z from 'zod';
+import {useForm} from 'react-hook-form'
+import EditorJS from "@editorjs/editorjs";
+import Header from '@editorjs/header'; 
+import { CloudCog } from "lucide-react";
+import Link from "next/link";
+
+import TextareaAutosize from "react-textarea-autosize"
+
+const DEFAULT_INITIAL_DATA =  {
+  "time": new Date().getTime(),
+  "blocks": [
+    {
+      "type": "header",
+      "data": {
+        "text": "This is my awesome editor!",
+        "level": 1
+      }
+    },
+  ]
+}
+
+const EditorComponent = ({post}) => {
+   const { register, handleSubmit } = useForm({
+    resolver: zodResolver(postPatchSchema),
+  })
+
+   
+  const ref = React.useRef()
+  const [isSaving, setIsSaving] = React.useState(false)
+  const [isMounted, setIsMounted] = React.useState(false)
+
+  const initializeEditor = React.useCallback(async () => {
+    const EditorJS = (await import("@editorjs/editorjs")).default
+    const Header = (await import("@editorjs/header")).default
+    const Embed = (await import("@editorjs/embed")).default
+    const Table = (await import("@editorjs/table")).default
+    const List = (await import("@editorjs/list")).default
+    const Code = (await import("@editorjs/code")).default
+    const LinkTool = (await import("@editorjs/link")).default
+    const InlineCode = (await import("@editorjs/inline-code")).default
+
+    // const body = postPatchSchema.parse(post)
+
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: "editor", 
+        onReady() {
+          ref.current = editor
+        },
+        placeholder: "Type here to write your post...",
+        inlineToolbar: true,
+        data: DEFAULT_INITIAL_DATA,
+        tools: {
+          header: Header,
+          linkTool: LinkTool,
+          list: List,
+          code: Code,
+          inlineCode: InlineCode,
+          table: Table,
+          embed: Embed,
+        },
+      })
+    }
+  }, [])
+
+
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMounted(true)
+    }
+  }, [])
+
+  React.useEffect(() => {
+    if (isMounted) {
+      initializeEditor()
+
+      return () => {
+        ref.current?.destroy()
+        ref.current = undefined
+      }
+    }
+  }, [isMounted, initializeEditor])
+
+  async function onSubmit(data) {
+    setIsSaving(true)
+
+    const blocks = await ref.current?.save()
+
+    console.log(data.title, blocks)
+
+    // const response = await fetch(`/api/posts/${post.id}`, {
+    //   method: "PATCH",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     title: data.title,
+    //     content: blocks,
+    //   }),
+    // })
+
+    setIsSaving(false)
+
+    // if (!response?.ok) {
+    //   return toast({
+    //     title: "Something went wrong.",
+    //     description: "Your post was not saved. Please try again.",
+    //     variant: "destructive",
+    //   })
+    // }
+
+    // router.refresh()
+
+    // return toast({
+    //   description: "Your post has been saved.",
+    // })
+  }
+    return  <>
+
+    <h2>Form</h2>
+    
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="grid w-full gap-10">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-center space-x-10">
+            <Link
+              href="/dashboard"
+              className={cn(buttonVariants({ variant: "ghost" }))}
+            >
+              <>
+                <Icons.chevronLeft className="mr-2 h-4 w-4" />
+                Back
+              </>
+            </Link>
+            <p className="text-sm text-slate-600">
+              {/* {post.published ? "Published" : "Draft"} */} "Post 1"
+            </p>
+          </div>
+          <button type="submit" className={cn(buttonVariants())}>
+            {isSaving && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            <span>Save</span>
+          </button>
+        </div>
+        <div className="prose prose-stone mx-auto w-[800px]">
+          <TextareaAutosize
+            autoFocus
+            id="title"
+            defaultValue="Post 1" //{post.title}
+            placeholder="Post title"
+            className="w-full resize-none appearance-none overflow-hidden text-5xl font-bold focus:outline-none"
+            {...register("title")}
+          />
+          <div id="editor" className="min-h-[500px]" />
+          <p className="text-sm text-gray-500">
+            Use{" "}
+            <kbd className="rounded-md border bg-slate-50 px-1 text-xs uppercase">
+              Tab
+            </kbd>{" "}
+            to open the command menu.
+          </p>
+        </div>
+      </div>
+    </form>
+    
+    </>;
+}
+
+export default EditorComponent;     
+
+
+const postPatchSchema  = z.object({
+  title: z.string('').min(3,'').max(128,'').optional(),
+
+  // TODO: Type this properly from editorjs block types?
+  content: z.any().optional(),
+})
+
+
+
+import {  clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs))
+}
+
+
+import { VariantProps, cva } from "class-variance-authority"
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-slate-900 text-white hover:bg-slate-700 dark:bg-slate-50 dark:text-slate-900",
+        destructive:
+          "bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600",
+        outline:
+          "bg-transparent border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100",
+        subtle:
+          "bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100",
+        ghost:
+          "bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent",
+        link: "bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent",
+      },
+      size: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 px-2 rounded-md",
+        lg: "h-11 px-8 rounded-md",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+
+
+
+
+import {
+  AlertTriangle,
+  ArrowRight,
+  Check,
+  ChevronLeft,
+  ChevronRight,
+  Command,
+  CreditCard,
+  File,
+  FileText,
+  Github,
+  HelpCircle,
+  Image,
+  Loader2,
+  LucideProps,
+  MoreVertical,
+  Pizza,
+  Plus,
+  Settings,
+  Trash,
+  Twitter,
+  User,
+  X,
+} from "lucide-react"
+
+
+export const Icons = {
+  logo: Command,
+  close: X,
+  spinner: Loader2,
+  chevronLeft: ChevronLeft,
+  chevronRight: ChevronRight,
+  trash: Trash,
+  post: FileText,
+  page: File,
+  media: Image,
+  settings: Settings,
+  billing: CreditCard,
+  ellipsis: MoreVertical,
+  add: Plus,
+  warning: AlertTriangle,
+  user: User,
+  arrowRight: ArrowRight,
+  help: HelpCircle,
+  pizza: Pizza,
+  gitHub: ({ ...props }) => (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="fab"
+      data-icon="github"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 496 512"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+      ></path>
+    </svg>
+  ),
+  twitter: Twitter,
+  check: Check,
+}
